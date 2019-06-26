f674ef714b8cde09736b8164c3e52ec453b75b3e
taifeitang <1026919284@qq.com>
2019-06-26 11:38:56 +0800

    bubble

---
+++ BubbleSort.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+
+public class BubbleSort{
+	/*从小到大冒泡排序*/
+	public  static int[] bubble(int[] arr){
+		int temp;
+		for(int i=0;i<arr.length-1;i++){
+			for(int j=0;j<arr.length-1-i;j++){
+				if(arr[j]>arr[j+1]){
+					temp=arr[j+1];
+					arr[j+1]=arr[j];
+					arr[j]=temp;
+				}
+				
+			}
+			
+		}
+		return arr;
+		
+	}
+	
+	public static void main(String[] args) {
+		int[] arr=new int[]{3,5,1,2,6,4};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+	}
+
+}
